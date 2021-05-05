8105c9f56d6dcb557c0075d6b2d6322d4c53e2af
artfulsage <artful.sage@yahoo.com>
2021-05-05 16:56:51 +0300

    feat: add function to get label for ngModel that is missing in current items

--- src/ng-select/lib/ng-select.component.ts
+++ src/ng-select/lib/ng-select.component.ts
@@ -24,8 +24,8 @@ import {
     Attribute
 } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
-import { takeUntil, startWith, tap, debounceTime, map, filter } from 'rxjs/operators';
-import { Subject, merge } from 'rxjs';
+import { takeUntil, startWith, tap, debounceTime, map, filter, take } from 'rxjs/operators';
+import { Subject, merge, Observable, isObservable } from 'rxjs';
 
 import {
     NgOptionTemplateDirective,
@@ -144,6 +144,8 @@ export class NgSelectComponent implements OnDestroy, OnChanges, AfterViewInit, C
     set clearSearchOnAdd(value) {
         this._clearSearchOnAdd = value;
     };
+
+    @Input() getMissingItemLabelFn: (value: any) => string | Observable<string> = () => null;
 
     // output events
     @Output('blur') blurEvent = new EventEmitter();
@@ -778,11 +780,28 @@ export class NgSelectComponent implements OnDestroy, OnChanges, AfterViewInit, C
                 if ((isValObject || isPrimitive)) {
                     this.itemsList.select(this.itemsList.mapItem(val, null));
                 } else if (this.bindValue) {
-                    item = {
-                        [this.bindLabel]: null,
-                        [this.bindValue]: val
+                    const selectItemWithLabelFn = (val: any, label: string) => {
+                        const item = {
+                            [this.bindLabel]: label,
+                            [this.bindValue]: val,
+                        };
+
+                        this.itemsList.select(this.itemsList.mapItem(item, null));
                     };
-                    this.itemsList.select(this.itemsList.mapItem(item, null));
+
+                    const label = this.getMissingItemLabelFn(val);
+
+                    if (isObservable(label)) {
+                        label.pipe(
+                            take(1),
+                            takeUntil(this._destroy$),
+                            tap(label => selectItemWithLabelFn(val, label)),
+                        )
+                        .subscribe();
+                    }
+                    else {
+                        selectItemWithLabelFn(val, label);
+                    }
                 }
             }
         };
